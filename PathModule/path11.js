a502e00da6b2575a4ba503376e14059125dbab81
MOHAMMAD ZEESHAN <mohammadzeeshan@gmail.com>
2024-01-29 23:03:48 +0530

    node js

--- PathModule/path11.js
+++ PathModule/path11.js
@@ -1,23 +1,23 @@
 const path = require('path');
 
 // directory name with path
-// console.log(path.dirname('G:\Node Js\PathModule\path.js'));
+// console.log(path.dirname('G:/NodeJs/PathModule/path.js'));
 
 // extension name with path
-// console.log(path.extname('G:\Node Js\PathModule\path.js'));
+// console.log(path.extname('G:/NodeJs/PathModule/path.js'));
 
 // basename with path
-// console.log(path.basename('G:\Node Js\PathModule\path.js'));
+// console.log(path.basename('G:/NodeJs/PathModule/path.js'));
 
-// console.log(path.normalize('G:\Node Js\PathModule\path.js'));
+// console.log(path.normalize('G:/NodeJs/PathModule/path.js'));
 
-// console.log(path.isAbsolute('G:\Node Js\PathModule\path.js'));
-
-
-// console.log(path.parse('G:\Node Js\PathModule\path.js'));
+// console.log(path.isAbsolute('G:/NodeJs/PathModule/path.js'));
 
 
-// const myPath = path.parse('G:\Node Js\PathModule\path.js');
+// console.log(path.parse('G:/NodeJs/PathModule/path.js'));
+
+
+// const myPath = path.parse('G:/NodeJs/PathModule/path.js');
 // console.log(myPath.name);
 // console.log(myPath.root);
 // console.log(myPath.ext);
