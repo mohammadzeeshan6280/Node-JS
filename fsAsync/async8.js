3f0cc55426a01b161d649a25e355395773a388e2
MOHAMMAD ZEESHAN <mohammadzeeshan@gmail.com>
2024-01-29 22:43:47 +0530

    update file

--- fsAsync/async8.js
+++ fsAsync/async8.js
@@ -1,9 +1,11 @@
 const fs = require("fs");
 
+//  Synchronous
 // const data = fs.readFileSync("read.txt", "utf-8")
 // console.log(data)
 // console.log("after the data")
 
+// Asynchronous
 fs.readFile("read.txt", "utf-8", (err, data) =>{
     console.log(data)
 })
